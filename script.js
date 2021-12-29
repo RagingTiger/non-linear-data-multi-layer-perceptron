d25d60990b69816bbef1400b31f641adf3fd7f06
Glitch (hello-webpage) <none>
2021-12-29 22:22:02 +0000

    🌔💑 Checkpoint ./script.js:9162534/181

--- script.js
+++ script.js
@@ -101,10 +101,9 @@ async function train() {
 
   // Finally do the training itself 
   let results = await model.fit(FEATURE_RESULTS.NORMALIZED_VALUES, OUTPUTS_TENSOR, {
-    validationSplit: 0.15, // Take aside 15% of the data to use for validation testing.
-    shuffle: true,         // Ensure data is shuffled before using in case it was in an order
-    batchSize: 64,         // As we have a lot of training data, batch size is set to 500.
-    epochs: 10             // Go over the data 100 times!
+    shuffle: true, // Ensure data is shuffled before using in case it was in an order
+    batchSize: 2,        
+    epochs: 200    // Go over the data 200 times!
   });
   
   OUTPUTS_TENSOR.dispose();
